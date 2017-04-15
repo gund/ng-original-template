0ab49dd999060264d73ac97e563809b3c3e4256a
gund <malkevich.alex@gmail.com>
2017-04-15 17:41:21 +0400

    fix: Remove config from module static method

--- src/original-template/original-template.module.ts
+++ src/original-template/original-template.module.ts
@@ -2,32 +2,18 @@ import { OriginalTemplateDirective } from './original-template.directive';
 import { OriginalTemplateService } from './original-template.service';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 
-export interface ModuleConfig {
-  maxPresiceIterations?: number;
-}
-
 @NgModule({
   declarations: [OriginalTemplateDirective],
   exports: [OriginalTemplateDirective]
 })
 export class OriginalTemplateModule {
 
-  static forRoot(config?: ModuleConfig): ModuleWithProviders {
-    if (config) {
-      OriginalTemplateModule._applyConfig(config);
-    }
-
+  static forRoot(): ModuleWithProviders {
     return {
       ngModule: OriginalTemplateModule,
       providers: [
         OriginalTemplateService
       ]
-    }
-  }
-
-  private static _applyConfig(config: ModuleConfig) {
-    if (Number.isInteger(config.maxPresiceIterations)) {
-      OriginalTemplateService.maxPreciseIterations = config.maxPresiceIterations;
     }
   }
 
